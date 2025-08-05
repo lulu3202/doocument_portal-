5f38e51c7e2705f7c40961121f41570e14ec32b4
Devi Priya K <priyamadurai2022@gmail.com>
2025-08-04 17:26:46 -0700

    folder structure updated with setup and init files

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="document_portal",
+    author="Devi Priya",
+    version="0.1",
+    packages=find_packages(),
+)
+
+#run "pip install -e . "
+#run "pip list"
